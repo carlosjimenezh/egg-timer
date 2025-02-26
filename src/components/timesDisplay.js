e4b4f02809ee8e401177144efa41b6ceb99acb31
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2025-02-25 19:34:35 -0600

    cambiar minutos por minutes

--- src/components/timesDisplay.js
+++ src/components/timesDisplay.js
@@ -2,10 +2,10 @@ import {setupTimers} from "../eggTimer"
 export default function timesDisplay () {
   document.querySelector('#display').innerHTML = `  
     <div id="times" class="times">
-      <button data-time="1">1 minuto</button>
-      <button data-time="5">5 minutos</button>
-      <button data-time="10">10 minutos</button>
-      <button data-time="15">15 minutos</button>
+      <button data-time="1">1 minute</button>
+      <button data-time="5">5 minutes</button>
+      <button data-time="10">10 minutes</button>
+      <button data-time="15">15 minutes</button>
     </div>
   `
 setupTimers(document.querySelectorAll(".times button"));
